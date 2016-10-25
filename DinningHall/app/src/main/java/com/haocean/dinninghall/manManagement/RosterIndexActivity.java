ff6eccd77da5233d2d4a8fde7f16c5f9c2bc6a19
liuluoping <973146368@qq.com>
2016-10-25 09:11:22 +0800

    删除工程

--- DinningHall/app/src/main/java/com/haocean/dinninghall/manManagement/RosterIndexActivity.java
+++
@@ -1,9 +0,0 @@
-package com.haocean.dinninghall.manManagement;
-
-import android.app.Activity;
-
-/**
- * Created by haocean on 2016/10/8.
- */
-public class RosterIndexActivity extends Activity {
-}
